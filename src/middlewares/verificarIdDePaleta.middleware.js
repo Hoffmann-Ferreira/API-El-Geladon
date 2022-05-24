29bce880056588b9c631462a6387c0bfa0881618
Hoffmann-Ferreira <andreihf@outlook.com>
2022-05-23 23:56:59 -0300

    Fix: Coreção de tratamento de erro

--- src/middlewares/verificarIdDePaleta.middleware.js
+++ src/middlewares/verificarIdDePaleta.middleware.js
@@ -8,7 +8,7 @@ const verificarIdDePaletaMiddleware = (req, res, next) => {
   if (!paleta) {
     return res
       .status(404)
-      .send('Paleta não encontrada, pois não foi cadastrada!');
+      .send('Paleta não encontrada!');
   }
 
   next();
