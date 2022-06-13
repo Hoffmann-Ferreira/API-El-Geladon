b1158a77e004ed4a1df3c5dc1689b207f03b0e84
Hoffmann-Ferreira <andreihf@outlook.com>
2022-06-13 20:46:12 -0300

    recfactor: conexão api

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import app from './app';
 import { connectDatabase } from './src/database';
 
 
-const port = process.env.PORT ||3005;
+const port = process.env.PORT || 3005;
 
 app.listen(port, () => {
   connectDatabase();
